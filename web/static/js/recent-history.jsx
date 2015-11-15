4c8fe28c16a9296b58a80d17cb2b3e61b41f94f4
onjiro <onjiro.mohyahya@gmail.com>
2015-11-15 21:58:49 +0900

    画面表示での金額の計算を正確に行うように修正

--- web/static/js/recent-history.jsx
+++ web/static/js/recent-history.jsx
@@ -1,12 +1,30 @@
 export default React.createClass({
+  debitItems(transaction) {
+    return "todo";
+  },
+  creditItems(transaction) {
+    return "todo";
+  },
+  debitSum(transaction) {
+    return _.chain(transaction.entries)
+      .select((e) => e.side_id == 1)
+      .reduce((memo, e) => memo + e.amount, 0)
+      .value();
+  },
+  creditSum(transaction) {
+    return _.chain(transaction.entries)
+      .select((e) => e.side_id == 2)
+      .reduce((memo, e) => memo + e.amount, 0)
+      .value();
+  },
   render() {
     var list =  this.props.data.map((transaction) => (
       <tr>
         <td>{moment(transaction.date).format('YYYY-MM-DD')}</td>
-        <td>todo</td>
-        <td>&yen;{transaction.entries[0].amount}</td>
-        <td>todo</td>
-        <td>&yen;{transaction.entries[1].amount}</td>
+        <td>{this.debitItems}</td>
+        <td>&yen;{this.debitSum(transaction)}</td>
+        <td>{this.creditItems}</td>
+        <td>&yen;{this.creditSum(transaction)}</td>
         <td></td>
       </tr>
     ));
