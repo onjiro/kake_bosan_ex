803930bcbd50fc43da3a056aba9dcbc472050455
onjiro <onjiro.mohyahya@gmail.com>
2016-03-27 22:44:04 +0900

    開発環境でローカルのユーザーを利用するように変更

--- config/dev.exs
+++ config/dev.exs
@@ -29,8 +29,6 @@ config :logger, :console, format: "[$level] $message\n"
 # Configure your database
 config :kake_bosan_ex, KakeBosanEx.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "kake_bosan_ex_dev",
   hostname: "localhost",
   size: 10 # The amount of database connections in the pool
